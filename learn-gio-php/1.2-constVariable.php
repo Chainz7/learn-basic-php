30e86ff643976db29c51fcc1e99cfb918e5ff4b9
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-02-12 23:55:14 +0700

    gio: 1.2

---
+++ learn-gio-php/1.2-constVariable.php
@@ -0,0 +1,32 @@
+<?php
+
+// Constants
+// $firstName = "Chainz";
+// $firstName = "Joe";
+// echo $firstName;
+
+// define('STATUS_PAID', 'paid');
+// define('STATUS_PAID', 'ya');
+
+// // boolean return 1
+// echo defined('STATUS_PAID');
+// // boolean return 0
+// echo defined('STATUS_VOID');
+
+// echo STATUS_PAID;
+
+// const STATUS_PAIDED = 'paided';
+
+// echo STATUS_PAIDED;
+
+// $paid = 'PAID';
+// define('STATUS_' . $paid, 4);
+// echo STATUS_PAID;
+
+// // Line Codes
+// echo __LINE__;
+
+// // Variable Variables
+// $foo = 'bar';
+// $$foo = 'baz';
+// echo "$foo, ${$foo}";
