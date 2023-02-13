87b617b12acc11ecae351dbd2b8fa69423dcdcd8
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-02-13 09:21:36 +0700

    gio: 1.3

---
+++ learn-gio-php/1.3-dataTypes.php
@@ -0,0 +1,42 @@
+<?php
+
+/* Data Types */
+
+# 4 Scalar Types 
+# bool - true / false
+$completed = true;
+# int - 1, 2, 3, 0, -5 
+$score = 75;
+# float - 1.5, 2.5
+$price = 0.99;
+# string
+$greeting = 'hello';
+
+echo $completed . "<br />";
+echo $score . "<br />";
+echo $price . "<br />";
+echo $greeting . "<br />";
+
+var_dump($completed);
+var_dump($score);
+var_dump($price);
+var_dump($greeting);
+
+# 4 Compound Types
+# array -
+$companies = [1, 2, 3, 0.5, -9.2, 'A', 'B', true];
+print_r($companies);
+# object - 
+# callable - 
+# iterable - 
+
+#2 Special Types
+# resource
+# null
+
+function sum($x, $y)
+{
+    return $x + $y;
+}
+
+echo sum(2, 3);
